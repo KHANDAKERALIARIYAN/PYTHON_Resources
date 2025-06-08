d377e1597223da8ec5ab1fb25fb3ebc17287878f
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-06-08 07:44:40 +0600

    added some new method

--- Python_Basics/3_list.py
+++ Python_Basics/3_list.py
@@ -5,7 +5,10 @@ numbers = [1, 2, 3, 4, 5]
 fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
 
 # Use a constructor
-# numbers2 = list((1, 2, 3, 4, 5))
+# numbers2 = list((1, 2, 3, 4, 5)) - previous version of Python
+
+# print fruits
+print(fruits)
 
 # Get a value
 print(fruits[1])
@@ -18,26 +21,41 @@ print(len(fruits))
 
 # Append to list
 fruits.append('Mangos')
+print(fruits)
 
 # Remove from list
 fruits.remove('Grapes')
+print(fruits)
 
 # Insert into position
 fruits.insert(2, 'Strawberries')
+print(fruits)
 
 # Change value
 fruits[0] = 'Blueberries'
+print(fruits)
 
 # Remove with pop
 fruits.pop(2)
+print(fruits)
 
 # Reverse list
 fruits.reverse()
+print(fruits)
 
 # Sort list
 fruits.sort()
+print(fruits)
 
 # Reverse sort
 fruits.sort(reverse=True)
+print(fruits)
+
+# clear list
+fruits.clear()
+print(fruits)
+
+# copy list
+fruits_copy = fruits.copy()
 
 print(fruits)
